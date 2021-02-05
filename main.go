fab719eb0909ef182d9f20dd51196d5ff7ea275e
Max <maxk893@gmail.com>
2021-02-04 20:23:03 -0800

    Updated getPublicIp function to pass in URLs to query for public IPs as parameters.

--- main.go
+++ main.go
@@ -6,20 +6,19 @@ import (
 	"net/http"
 )
 
-func getPublicIp() (string, string) {
+func getPublicIp(ip4Url, ip6Url string) (string, string) {
 	// Get public IPv4 address of host
-	reqV4, _ := http.Get("https://api.ipify.org")
+	reqV4, _ := http.Get(ip4Url)
 	ipV4, _ := ioutil.ReadAll(reqV4.Body)
 
 	// Get public IPv6 address of host
-	reqV6, _ := http.Get("https://api6.ipify.org")
+	reqV6, _ := http.Get(ip6Url)
 	ipV6, _ := ioutil.ReadAll(reqV6.Body)
 
 	return string(ipV4), string(ipV6)
 }
 
 func main() {
-	ipV4, ipV6 := getPublicIp()
+	ipV4, ipV6 := getPublicIp("https://api.ipify.org", "https://api6.ipify.org")
 	fmt.Println(fmt.Sprintf("IPv4: %s \nIPv6: %s", ipV4, ipV6))
-
 }
