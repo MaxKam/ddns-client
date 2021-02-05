a8afb54600b23281c25811b1b319198d6cff17c7
Max <maxk893@gmail.com>
2021-02-04 22:49:54 -0800

    Added error checking to getPublicIP function.

--- main.go
+++ main.go
@@ -4,17 +4,35 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 // Function to get public IPv4 and IPv6 IPs of host
 func getPublicIP(ip4Url, ip6Url string) (string, string) {
 	// Get public IPv4 address of host
-	reqV4, _ := http.Get(ip4Url)
-	ipV4, _ := ioutil.ReadAll(reqV4.Body)
+	reqV4, err := http.Get(ip4Url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not get public IPv4 address: %v\n", err)
+		os.Exit(1)
+	}
 
+	ipV4, err := ioutil.ReadAll(reqV4.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not read IPv4 response: %v\n", err)
+		os.Exit(1)
+	}
 	// Get public IPv6 address of host
-	reqV6, _ := http.Get(ip6Url)
-	ipV6, _ := ioutil.ReadAll(reqV6.Body)
+	reqV6, err := http.Get(ip6Url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not get public IPv6 address: %v\n", err)
+		os.Exit(1)
+	}
+
+	ipV6, err := ioutil.ReadAll(reqV6.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not read IPv6 response: %v\n", err)
+		os.Exit(1)
+	}
 
 	return string(ipV4), string(ipV6)
 }
