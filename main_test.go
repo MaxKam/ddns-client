09c4db5f4439cabffa977e15648da77ae159d249
Max <maxk893@gmail.com>
2021-02-04 22:30:02 -0800

    Added unit tests.

---
+++ main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetPublicIP(t *testing.T) {
+	const (
+		ip4Address string = "192.168.1.1"
+		ip6Address string = "2001:0db8:85a3:0000:0000:8a2e:0370:7334" // Sample IPv6 address found on Wikipedia
+	)
+
+	// Create a new Serv Mux so that we can attach multiple routes to the httptest server
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/ipv4", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, ip4Address)
+	})
+
+	mux.HandleFunc("/ipv6", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, ip6Address)
+	})
+
+	ts := httptest.NewServer(mux)
+	defer ts.Close()
+
+	// Use stub server to test function
+	ans1, ans2 := getPublicIP(ts.URL+"/ipv4", ts.URL+"/ipv6")
+
+	if ans1 != ip4Address && ans2 != ip6Address {
+		t.Errorf("Did not receive expected IPs")
+	}
+
+	ts.Close()
+
+}
