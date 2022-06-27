a38e2b9f687e8b3aef9ff72774f65b07a1293dee
Max K <maxk893@gmail.com>
2022-06-26 22:22:13 -0500

    Updated tests

--- main_test.go
+++ main_test.go
@@ -28,10 +28,17 @@ func TestGetPublicIP(t *testing.T) {
 	defer ts.Close()
 
 	// Use stub server to test function
-	ans1, ans2 := getPublicIP(ts.URL+"/ipv4", ts.URL+"/ipv6")
+	ans1 := getPublicIP(ts.URL + "/ipv4")
 
-	if ans1 != ip4Address && ans2 != ip6Address {
-		t.Errorf("Did not receive expected IPs")
+	if ans1 != ip4Address {
+		t.Errorf("Did not receive expected IPv4")
+	}
+
+	// Use stub server to test function
+	ans2 := getPublicIP(ts.URL + "/ipv6")
+
+	if ans2 != ip6Address {
+		t.Errorf("Did not receive expected IPv6")
 	}
 
 	ts.Close()
@@ -42,7 +49,8 @@ func TestCheckIPsMatch(t *testing.T) {
 	publicIPv4, domainIPv4 := "192.168.1.1", "192.168.1.1"
 	publicIPv6, domainIPv6 := "2001:0db8:85a3:0000:0000:8a2e:0370:7334", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"
 
-	resultIPv4, resultIPv6 := checkIPsMatch(publicIPv4, domainIPv4, publicIPv6, domainIPv6)
+	resultIPv4 := checkIPsMatch(publicIPv4, domainIPv4)
+	resultIPv6 := checkIPsMatch(publicIPv6, domainIPv6)
 
 	if resultIPv4 != true && resultIPv6 != true {
 		t.Errorf("Failed to detect that public and domain IPs match")
@@ -54,7 +62,8 @@ func TestCheckIPsMatchFalse(t *testing.T) {
 	publicIPv4, domainIPv4 := "192.168.1.1", "192.168.10.10"
 	publicIPv6, domainIPv6 := "2001:0db8:85a3:0000:0000:8a2e:0370:7334", "0000:0db8:85a3:0000:0000:8a2e:0370:0000"
 
-	resultIPv4, resultIPv6 := checkIPsMatch(publicIPv4, domainIPv4, publicIPv6, domainIPv6)
+	resultIPv4 := checkIPsMatch(publicIPv4, domainIPv4)
+	resultIPv6 := checkIPsMatch(publicIPv6, domainIPv6)
 
 	if resultIPv4 != false && resultIPv6 != false {
 		t.Errorf("Failed to detect that public and domain IPs don't match")
